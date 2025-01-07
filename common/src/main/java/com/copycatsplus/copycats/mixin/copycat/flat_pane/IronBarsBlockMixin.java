a8bea9e364b48e6718a321ba14f44ad8272ee853
Henry Lin <henry.ys.lin@gmail.com>
2025-01-07 10:53:02 +0800

    Panes should only connect to flat panes in the correct orientation

--- common/src/main/java/com/copycatsplus/copycats/mixin/copycat/flat_pane/IronBarsBlockMixin.java
+++ common/src/main/java/com/copycatsplus/copycats/mixin/copycat/flat_pane/IronBarsBlockMixin.java
@@ -1,27 +1,30 @@
 package com.copycatsplus.copycats.mixin.copycat.flat_pane;
 
 import com.copycatsplus.copycats.content.copycat.flat_pane.CopycatFlatPaneBlock;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.Direction;
+import net.minecraft.core.Direction.Axis;
 import net.minecraft.world.level.block.IronBarsBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
  * Allow normal panes to attach to copycat flat panes
  */
 @Mixin(IronBarsBlock.class)
 public class IronBarsBlockMixin {
-    @Inject(
-            method = "attachsTo",
-            at = @At("HEAD"),
-            cancellable = true
+    @WrapOperation(
+            method = "updateShape",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/IronBarsBlock;attachsTo(Lnet/minecraft/world/level/block/state/BlockState;Z)Z")
     )
-    private void attachesToCopycat(BlockState state, boolean solidSide, CallbackInfoReturnable<Boolean> cir) {
-        if (state.getBlock() instanceof CopycatFlatPaneBlock && state.getValue(BlockStateProperties.AXIS) != Direction.Axis.Y)
-            cir.setReturnValue(true);
+    private boolean attachsToCopycat(IronBarsBlock instance, BlockState state, boolean solidSide, Operation<Boolean> original, @Local(argsOnly = true) Direction direction) {
+        if (state.getBlock() instanceof CopycatFlatPaneBlock) {
+            Axis axis = state.getValue(CopycatFlatPaneBlock.AXIS);
+            return axis != Axis.Y && axis != direction.getAxis();
+        }
+        return original.call(instance, state, solidSide);
     }
 }
