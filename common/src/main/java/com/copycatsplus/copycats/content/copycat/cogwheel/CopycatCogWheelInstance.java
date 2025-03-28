12f6902f30dbff64b9bcca1a09f0cbf6295c57a9
Henry Lin <henry.ys.lin@gmail.com>
2025-03-28 21:03:31 +0800

    Model update logic for multi-state

--- common/src/main/java/com/copycatsplus/copycats/content/copycat/cogwheel/CopycatCogWheelInstance.java
+++ common/src/main/java/com/copycatsplus/copycats/content/copycat/cogwheel/CopycatCogWheelInstance.java
@@ -2,6 +2,7 @@ package com.copycatsplus.copycats.content.copycat.cogwheel;
 
 import com.copycatsplus.copycats.CCCopycatPartialModels;
 import com.copycatsplus.copycats.content.copycat.shaft.CopycatShaftBlock;
+import com.copycatsplus.copycats.foundation.copycat.model.kinetic.ICopycatPartialModel;
 import com.copycatsplus.copycats.foundation.copycat.model.kinetic.KineticCopycatRenderData;
 import com.copycatsplus.copycats.foundation.copycat.model.kinetic.KineticCopycatRenderer;
 import com.simibubi.create.content.kinetics.base.KineticBlockEntityVisual;
@@ -12,11 +13,14 @@ import dev.engine_room.flywheel.api.instance.Instance;
 import dev.engine_room.flywheel.api.visualization.VisualizationContext;
 import net.minecraft.core.Direction;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.function.Consumer;
 
 public class CopycatCogWheelInstance extends KineticBlockEntityVisual<CopycatCogWheelBlockEntity> {
-    protected final RotatingInstance rotatingShaft;
-    protected final RotatingInstance rotatingCogwheel;
+    protected Map<String, CopycatRotatingInstance> rotatingModels = new HashMap<>();
+    protected Direction from;
 
     public CopycatCogWheelInstance(VisualizationContext context, CopycatCogWheelBlockEntity blockEntity, float partialTick) {
         this(
@@ -33,54 +37,75 @@ public class CopycatCogWheelInstance extends KineticBlockEntityVisual<CopycatCog
      */
     public CopycatCogWheelInstance(VisualizationContext context, CopycatCogWheelBlockEntity blockEntity, float partialTick, Direction from) {
         super(context, blockEntity, partialTick);
-        rotatingShaft = instancerProvider()
-                .instancer(AllInstanceTypes.ROTATING, KineticCopycatRenderer.getInstancedModel(
-                        CCCopycatPartialModels.SHAFT,
-                        blockEntity,
-                        CopycatCogWheelBlock.Part.SHAFT.getSerializedName()
-                ))
-                .createInstance()
-                .rotateToFace(from, rotationAxis())
-                .setup(blockEntity)
-                .setPosition(getVisualPosition());
-        rotatingCogwheel = instancerProvider()
-                .instancer(AllInstanceTypes.ROTATING, KineticCopycatRenderer.getInstancedModel(
-                        ICogWheel.isLargeCog(blockEntity.getBlockState()) ? CCCopycatPartialModels.LARGE_COGWHEEL : CCCopycatPartialModels.COGWHEEL,
-                        blockEntity,
-                        CopycatCogWheelBlock.Part.COGWHEEL.getSerializedName()
-                ))
-                .createInstance()
-                .rotateToFace(from, rotationAxis())
-                .setup(blockEntity)
-                .setPosition(getVisualPosition());
+        this.from = from;
+        addModel(CopycatCogWheelBlock.Part.SHAFT.getSerializedName());
+        addModel(CopycatCogWheelBlock.Part.COGWHEEL.getSerializedName());
+    }
 
-        rotatingShaft.setChanged();
-        rotatingCogwheel.setChanged();
+    private CopycatRotatingInstance createModel(String key) {
+        ICopycatPartialModel partialModel;
+        if (key.equals(CopycatCogWheelBlock.Part.SHAFT.getSerializedName())) {
+            partialModel = CCCopycatPartialModels.SHAFT;
+        } else {
+            partialModel = ICogWheel.isLargeCog(blockEntity.getBlockState())
+                    ? CCCopycatPartialModels.LARGE_COGWHEEL
+                    : CCCopycatPartialModels.COGWHEEL;
+        }
+        return CopycatRotatingInstance.of(
+                KineticCopycatRenderData.of(partialModel, blockEntity, key),
+                instancerProvider()
+                        .instancer(AllInstanceTypes.ROTATING, KineticCopycatRenderer.getInstancedModel(
+                                partialModel,
+                                blockEntity,
+                                key
+                        ))
+                        .createInstance()
+                        .rotateToFace(from, rotationAxis())
+                        .setup(blockEntity)
+                        .setPosition(getVisualPosition())
+        );
+    }
+
+    private void addModel(String key) {
+        CopycatRotatingInstance newInstance = createModel(key);
+        newInstance.rotatingInstance.setChanged();
+        rotatingModels.put(key, newInstance);
     }
 
     @Override
     public void update(float pt) {
-        rotatingShaft.setup(blockEntity)
-                .setChanged();
-        rotatingCogwheel.setup(blockEntity)
-                .setChanged();
+        for (Map.Entry<String, CopycatRotatingInstance> entry : rotatingModels.entrySet()) {
+            KineticCopycatRenderData renderData = entry.getValue().renderData;
+            if (!renderData.state().equalsState(blockEntity.getBlockState()) || !renderData.material().equals(blockEntity.getMaterialItemStorage().getMaterialItem(entry.getKey()).material())) {
+                entry.getValue().rotatingInstance().delete();
+                CopycatRotatingInstance newInstance = createModel(entry.getKey());
+                entry.setValue(newInstance);
+                newInstance.rotatingInstance().setChanged();
+            } else {
+                entry.getValue().rotatingInstance().setup(blockEntity).setChanged();
+            }
+        }
     }
 
     @Override
     public void updateLight(float partialTick) {
-        relight(rotatingShaft);
-        relight(rotatingCogwheel);
+        rotatingModels.forEach((k, v) -> relight(v.rotatingInstance()));
     }
 
     @Override
     protected void _delete() {
-        rotatingShaft.delete();
-        rotatingCogwheel.delete();
+        rotatingModels.forEach((k, v) -> v.rotatingInstance().delete());
     }
 
     @Override
     public void collectCrumblingInstances(Consumer<Instance> consumer) {
-        consumer.accept(rotatingShaft);
-        consumer.accept(rotatingCogwheel);
+        rotatingModels.forEach((k, v) -> consumer.accept(v.rotatingInstance()));
+    }
+
+    public static record CopycatRotatingInstance(KineticCopycatRenderData renderData,
+                                                 RotatingInstance rotatingInstance) {
+        public static CopycatRotatingInstance of(KineticCopycatRenderData renderData, RotatingInstance rotatingInstance) {
+            return new CopycatRotatingInstance(renderData, rotatingInstance);
+        }
     }
 }
