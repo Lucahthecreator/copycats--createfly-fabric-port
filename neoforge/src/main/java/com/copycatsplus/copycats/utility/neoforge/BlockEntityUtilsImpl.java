4745f737bbde060d9886641e2e62097072951fe1
Bennyboy1695 <benny.boy1695@gmail.com>
2025-06-03 01:06:26 +0100

    Don't update light in the schematic world to stop breaking schematics

--- neoforge/src/main/java/com/copycatsplus/copycats/utility/neoforge/BlockEntityUtilsImpl.java
+++ neoforge/src/main/java/com/copycatsplus/copycats/utility/neoforge/BlockEntityUtilsImpl.java
@@ -1,6 +1,7 @@
 package com.copycatsplus.copycats.utility.neoforge;
 
 import com.copycatsplus.copycats.utility.BlockEntityUtils;
+import net.createmod.catnip.levelWrappers.SchematicLevel;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -13,6 +14,8 @@ public class BlockEntityUtilsImpl {
 
     public static void updateLight(BlockEntity blockEntity) {
         Level level = blockEntity.getLevel();
+        if (level instanceof SchematicLevel)
+            return;
         if (level != null && level.getChunkSource() != null) { // should not be necessary to check chunk source for null but seems like it happens inconsistently with other mods installed
             BlockPos pos = blockEntity.getBlockPos();
             AuxiliaryLightManager lightManager = level.getAuxLightManager(pos);
